b6962e284c963fc4af1a29cb86b3908d007e5b91
James Dapp <jfunk1994@yahoo.com>
2015-10-24 03:22:35 -0400

    Skeleton of packet type switch.

--- LasertagPacketSensor/src/main.c
+++ LasertagPacketSensor/src/main.c
@@ -31,6 +31,8 @@
 #include "main.h"
 #include <stdint.h>
 
+typedef enum { false, true } bool;
+
 /** @addtogroup STM32F3_Discovery_Peripheral_Examples
   * @{
   */
@@ -61,10 +63,122 @@ int main(void)
 
 	//Begin defining packet types
 
-	int packet=0;
+	 bool packet[100];
+	 int ptype=0;
 
-	switch(packet);
-	while(1);
+//not finished implementing switch structure, currently only a shell
+	 switch(ptype)
+	 {
+	 case 0: //0x000 countdown
+		{
+			break;
+		}
+	 case 1: //0x001 Assign Player
+		{
+			break;
+		}
+	 case 2: //0x002 Announce Custom Lazer Tag Game
+		{
+			break;
+		}
+	 case 3: //0x003 Announce Custom Laser Tag (2 Teams) Game
+		{
+			break;
+		}
+	 case 4: //0x004 Announce Custom Lazer Tag (3 Teams) Game
+		{
+			break;
+		}
+	 case 5: //0x005 Announce Hide And seek Game
+		{
+			break;
+		}
+	 case 6: //0x006 Announce Hunt the Prey Game
+		{
+			break;
+		}
+	 case 7: //0x007 Announce Kings (2 Teams) Game
+		{
+			break;
+		}
+	 case 8: //0x008 Announce Kings (3 Teams) Game
+		{
+			break;
+		}
+	 case 9: //0x009 Announce Own the Zone Game
+		{
+			break;
+		}
+	 case 10: //0x00A Announce Own the Zone (2 Teams) Game
+		{
+			break;
+		}
+	 case 11: //0x00B Announce Own the Zone (3 Teams) Game
+		{
+			break;
+		}
+	 case 12: //0x00C Announce Special Game
+		{
+			break;
+		}
+	 case 15: //0x00F Assign Player Fail
+		{
+			break;
+		}
+	 case 16: //0x010 Request Join Game
+		{
+			break;
+		}
+	 case 17: //0x011 Acknowledge Player Assignment
+		{
+			break;
+		}
+	 case 32: //0x020 Request Assistance
+		{
+			break;
+		}
+	 case 33: //0x021 Send assistance
+		{
+			break;
+		}
+	 case 49: //0x031 Request Tag Report
+		{
+			break;
+		}
+	 case 50: //0x032 Rank Report
+		{
+			break;
+		}
+	 case 64: //0x040 Tag Summary
+		{
+			break;
+		}
+	 case 65: //0x041 Team 1 Tag Report
+		{
+			break;
+		}
+	 case 66: //0x042 Team 2 Tag Report
+		{
+			break;
+		}
+	 case 67: //0x043 Team 3 Tag Report
+		{
+			break;
+		}
+	 case 72: //0x048 Single Tag Report
+		{
+			break;
+		}
+	 case 128: //0x080 Text Message
+		{
+			break;
+		}
+	 case 144: //0x090 Special Attack
+		{
+			break;
+		}
+	 }
+	 while(1);
 
 }
 
