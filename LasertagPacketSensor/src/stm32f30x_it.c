4ab39b20453926e003ae1c1f12b0f5db662f3bef
adam <scottae@clarkson.edu>
2016-03-09 21:08:39 -0500

    ima pushin

--- LasertagPacketSensor/src/stm32f30x_it.c
+++ LasertagPacketSensor/src/stm32f30x_it.c
@@ -144,7 +144,7 @@ void PendSV_Handler(void)
   */
 void SysTick_Handler(void)
 {
-  TimingDelay_Decrement();
+  //TimingDelay_Decrement();
 }
 
 /******************************************************************************/
